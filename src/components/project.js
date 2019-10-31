f40b5f47cdaad958d333a92b967b5259396af41d
Nicolas <info@nicolas-moyano.se>
2019-10-31 15:04:57 +0100

    changes alt tags to projects

--- src/components/project.js
+++ src/components/project.js
@@ -8,7 +8,7 @@ import readArrow from '../images/ReadArrow.svg'
 const SelectedProject = () =>(
     <div className="Project" style={{ maxWidth: 1200, marginTop: '160px'}}>
         <div style={{ textAlign: 'center', marginBottom: '80px' }} > <h5>Selected work</h5></div>
-        <div><img src={GodelBG} alt="test poject"></img>
+        <div><img src={GodelBG} alt="Godel"></img>
             <div className="project-info" style={{ position: 'relative', top: '-20vh', marginLeft: '50%', width:'590px', backgroundColor: '#FFFFFF', padding: '40px', boxShadow: '0px 8px 20px 0px rgba(0,0,0,0.1)'}}>
                 <h2>Godel</h2>
                 <p>There is one thing that all electricity companies must do: sell electricity. So we do as well. But our profits goes to organizations that tries to save the world.</p>
@@ -21,7 +21,7 @@ const SelectedProject = () =>(
             </div>
         </div>
 
-        <div><img src={IcaBG} alt="test poject"></img>
+        <div><img src={IcaBG} alt="Ica handla app"></img>
             <div className="project-info" style={{ position: 'relative', top: '-20vh', marginLeft: '50%', width:'590px', backgroundColor: '#FFFFFF', padding: '40px', boxShadow: '0px 8px 20px 0px rgba(0,0,0,0.1)'}}>
                 <h2>Ica handla</h2>
                 <p>The supermarket industry is a saturated market, where customer loyalty is hard to acquire, and even harder to maintain. Building one of Sweden’s best-loved apps </p>
